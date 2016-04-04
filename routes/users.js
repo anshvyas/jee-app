c1b9c41f4fb144cabbddea29eb3b1ac2a3673f1f
Anshul Vyas <anshul.vyas380@gmail.com>
2016-04-04 14:41:54 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var mongojs=require('mongojs');
+var ObjectId = require('mongodb').ObjectID;
+var url='mongodb://localhost:27017/appdb';
+var db=mongojs("appdb",['users']);
+/*var MongoClient=require('mongodb').MongoClient;
+var assert=require ('assert');*/
 /* GET users listing. */
 router.route('/register_1').
 	post(function(req, res, next) {
@@ -8,18 +13,34 @@ router.route('/register_1').
 	  var name=req.body.name;
 	  var email=req.body.email;
 	  var phoneno=req.body.phoneno;
-
+       var password=req.body.password
 	  // create the json object to enter into db
 	  var user_data={
 	  	'name':name,
 	  	'email':email,
-	  	'phoneno':phoneno
+	  	'phoneno':phoneno,
+	  	'password':password,
+	  	'standard':null,
+	  	'question_frequency' :null,
+	  	'join_date':null,
+	  	'batch':null
+	  	
 	  };
 
-	  res.send({'status':'registered','user_data':user_data});
-
+	db.users.insert(user_data,function(err,save){
+		if(err){
+			console.log(err);
+		}
+		else {
+			console.log(save);
+			res.send({'status':'registered','user_data':save});
+		}
 	});
+	  
+	});
+	
 
+       
 router.route('/register_2')
 	.post(function(req,res,next){
 		
@@ -27,18 +48,50 @@ router.route('/register_2')
 		var d=new Date();
 		var standard=req.body.standard;
 		var question_frequency=req.body.question_frequency;
-		var join_date=d.getDate()+'/'+d.getMonth()+'/'+d.getFullYear();
-
+		var month=d.getMonth()+1;
+		var join_date=d.getDate()+'/'+month+'/'+d.getFullYear();
+        var id=req.body.id;
 		// create a json
-		var user_data={
+		var user_data_2={
+
 			'standard':standard,
 			'question_frequency':question_frequency,
 			'join_date':join_date,
 			'batch':1
 		};
-
-		res.send({'status':'registered','user_data':user_data});
-
+          db.users.update({'_id':ObjectId(id)},{$set:user_data_2},function(err,save){
+          	if(err){
+			console.log(err);
+		}
+		else {
+			console.log(save);
+			res.send({'status':'registered','user_data':save});
+		}
+	}); 
+		//res.send({'status':'registered','user_data':user_data_2});
+        
 	});	
+router.route('/login').post(function(req,res,next){
+		var email=req.body.email;
+		var password=req.body.password;
+		db.users.findOne({'email':email},function(err,save){
+			if(save&& !err){
+				console.log(save.password);
+				//var user=JSON.parse(save);
+				//console.log(user.password);
+                   if(save.password==password)
+                   {
+
+                   	res.send({'status':1,'message':save});
+                   }
+                   else
+                   	res.send({'status':0,'message':'sorry wrong password'});
+			}
+			else{
+                 res.send({'status':0,'message':'sorry you are not a registered user'});
+			}
+
+		})
+	})	
 
 module.exports = router;
